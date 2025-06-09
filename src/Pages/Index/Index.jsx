ba57d48807a7352694f93e01ac4327918b42ee09
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-06-09 21:13:08 +0545

    Add new PNG assets for homepage revamp (rev_home3_05.png and rev_home3_06.png)

--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Nav from "../../Components/Nav/Nav";
 
-function Index(){
-    return(
-        <>
-        <Nav/>
-        </>
+function Index() {
+    return (
+        <div className="flex flex-col gap-5 relative min-h-[90vh] bg-[#ffded1]">
+            <Nav />
+            <div>
+                <div></div>
+                <div>
+                    <img src={heroImage} alt="" />
+                </div>
+            </div>
+        </div>
     )
 }
 export default Index;
